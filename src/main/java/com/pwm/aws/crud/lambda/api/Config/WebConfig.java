2f49a607b7ea25016d3dfb85430e7745550e3494
Chris Costa <costachristopher90@gmail.com>
2024-06-27 18:15:35 -0400

    got h2 databse console working still 404 error

--- src/main/java/com/pwm/aws/crud/lambda/api/Config/WebConfig.java
+++ src/main/java/com/pwm/aws/crud/lambda/api/Config/WebConfig.java
@@ -1,16 +1,29 @@
 package com.pwm.aws.crud.lambda.api.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(false);
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        System.out.println("CORS configuration: " + configuration);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/pwm/aws/crud/lambda/api/Controller/ProductSpringController.java
+++ src/main/java/com/pwm/aws/crud/lambda/api/Controller/ProductSpringController.java
@@ -3,13 +3,16 @@ package com.pwm.aws.crud.lambda.api.Controller;
 import com.pwm.aws.crud.lambda.api.Service.ProductService;
 import com.pwm.aws.crud.lambda.api.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
-@RequestMapping("/contract")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "Content-Type")
+@RequestMapping("/api")
 public class ProductSpringController {
     @Autowired
     private ProductService productService;
@@ -19,9 +22,12 @@ public class ProductSpringController {
         return productService.getAllProducts();
     }
 
-    @PostMapping
-    public Product createProduct(@RequestBody Product product) {
-        return productService.saveProduct(product);
+    @PostMapping("/contract")
+    public ResponseEntity<Map<String, String>> saveProduct(@RequestBody Product product) {
+        System.out.println("Received contact request: " + product);
+        productService.saveProduct(product);
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "New Contact Registered");
+        return ResponseEntity.ok(response);
     }
-
 }
